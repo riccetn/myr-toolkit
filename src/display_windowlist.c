59778543e030f093e076f30958539dbfbacb6f00
Rickard Närström <rickard@narstrom.se>
2016-12-02 10:47:15 +0100

    Added a stub MrDisplayWindowList.
    The new class will be a hashtable for mapping window XID's
    (xcb_window_t) to MyrWindow*

---
+++ src/display_windowlist.c
@@ -0,0 +1,55 @@
+/*
+ * Written 2016 by Rickard Närström <rickard@narstrom.se>
+ *
+ * This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "display_windowlist.h"
+
+typedef struct node Node;
+
+struct node {
+	Node *next;
+	MyrWindow *window;
+};
+
+struct myr_display_window_list {
+	Node **table;
+	int nbuckets;
+	int nnodes;
+};
+
+MyrDisplayWindowList *myr_display_window_list_create(void) {
+	return NULL;
+}
+
+void myr_display_window_list_free(MyrDisplayWindowList *list) {
+}
+
+MyrWindow *myr_display_window_list_lookup(MyrDisplayWindowList *list, xcb_window_t xwindowid) {
+	return NULL;
+}
+
+void myr_display_window_list_add(MyrDisplayWindowList *list, xcb_window_t xwindowid, MyrWindow *window) {
+}
